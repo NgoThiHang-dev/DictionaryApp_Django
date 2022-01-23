988b047cc77923d456ab0a56bf44c069c806899d
Hang Ngo Thi <1710166@dlu.edu.vn>
2022-01-23 16:16:38 +0700

    Update translate document

--- khodictionary/translate/views.py
+++ khodictionary/translate/views.py
@@ -7,6 +7,7 @@ from django.http import HttpResponse
 from django.db.models import Q
 from django.shortcuts import render, HttpResponse
 from dictionary.models import *
+from django.core.files.storage import FileSystemStorage
 # from googletrans import Translator
 
 
@@ -30,5 +31,10 @@ def translate(request):
 
 
 def document(request):
-    
+    if request.method == 'POST' and request.FILES['upload']:
+        upload = request.FILES['upload']
+        fss = FileSystemStorage()
+        file = fss.save(upload.name, upload)
+        file_url = fss.url(file)
+        return render(request, 'translate/document.html', {'file_url': file_url})
     return render(request, 'translate/document.html')
